8d0fac5528f0d16488a5e9670be74756d49cdd57
night-fury007 <souptikmondal.edu@gmail.com>
2025-02-01 10:21:26 +0530

    initial commit

---
+++ src/main/java/com/graphql/graphql_project1/GraphqlProject1Application.java
@@ -0,0 +1,13 @@
+package com.graphql.graphql_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphqlProject1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/graphql/graphql_project1/GraphqlProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphql.graphql_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
